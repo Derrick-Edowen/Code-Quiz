812966d99d6fa1db524e6f3689e3d4d6c3f747c6
Derrick Edowen <derrickedowen@hotmail.com>
2022-09-25 18:37:01 -0400

    Added HTML, CSS, JS files. Added basic code for HTML and CCS

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+
+var questions = {
+    q1: "Commonly used data types DO Not include:",
+    q2: "The condition in an  if/else statement is enclosed with______.",
+    q3: "Arrays in JavaScript can be  used to store ______.",
+    q4: "String values must be  enclosed within ______ when being assigned to variables.",
+    q5: "A very useful tool  used during development and debugging for printing content to the debugger is:"
+};
+var score = 0;
+var timeLeft = 60;
+
+
+//for (var i = 0; i <questions.length; i++) {
+
+//}
+//function timer() {
+
+//}
+console.log(questions.length)
