ef4674f260c47c38fd6604ef85ca16d08e26659e
Derrick Edowen <derrickedowen@hotmail.com>
2022-09-26 00:14:17 -0400

    Added CSS styling to CSS file. Added basic logic and variables to JavaScript. Organized and  sectioned HTML code.

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,85 @@
 
-var questions = {
-    q1: "Commonly used data types DO Not include:",
-    q2: "The condition in an  if/else statement is enclosed with______.",
-    q3: "Arrays in JavaScript can be  used to store ______.",
-    q4: "String values must be  enclosed within ______ when being assigned to variables.",
-    q5: "A very useful tool  used during development and debugging for printing content to the debugger is:"
-};
-var score = 0;
-var timeLeft = 60;
+var questions = [
+    {
+        question: 'Commonly used data types DO Not Include: ',
+        answers: [
+            {ID: 1, answer:'1. Strings'},
+            {ID: 2, answer:'2. Booleans'},
+            {ID: 3, answer:'3. Alerts'},
+            {ID: 4, answer:'4. Numbers'},
+        ],
+        correct: 3,
+        selected: null,
+    },
+    {
+        question: 'The condition in an if/else statement is enclosed with______. ',
+        answers: [
+            {ID: 5, answer:'1. Quotes'},
+            {ID: 6, answer:'2. Curly Brackets'},
+            {ID: 7, answer:'3. Parenthesis'},
+            {ID: 8, answer:'4. Square Brackets'},
+        ],
+        correct: 7,
+        selected: null,
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store ________.',
+        answers: [
+            {ID: 9, answer:'1. Numbers and Strings'},
+            {ID: 10, answer:'2. Other Arrays'},
+            {ID: 11, answer:'3. Booleans'},
+            {ID: 12,answer:'4. All of the Above'},
+    ],
+    correct: 12,
+    selected: null,
+    },
+    {
+        question: 'String values must be enclosed within ________ whenbeing assigned to variables.',
+        answers: [
+            {ID: 13, answer:'1. Commas'},
+            {ID: 14, answer:'2. Curly Brackets'},
+            {ID: 15, answer:'3. Quotes'},
+            {ID: 16, answer:'4. Parenthesis'},
+        ],
+        corrected: 15,
+        selected: null,
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            {ID: 17, answer:'1. JavaScript'},
+            {ID: 18, answer:'2. Terminal/Bash'},
+            {ID: 19, answer:'3. For Loops'},
+            {ID: 20, answer:'4. Console log'},
+        ],
+        corrected: 20,
+        selected: null,
+    },
+];
+
+var Timer = 45;
+var Timercount;
+var response = document.getElementById('result');
 
 
-//for (var i = 0; i <questions.length; i++) {
 
-//}
-//function timer() {
 
-//}
-console.log(questions.length)
+
+
+function populateQuestionDet() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
